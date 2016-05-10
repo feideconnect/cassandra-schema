060bfb5131eae490d3fbf36e0b822366c3f312f0
Sigmund Augdal <sigmund.augdal@uninett.no>
2016-05-10 12:09:45 +0200

    Fix migration of other keyspaces than the default when not using migrate wrapper script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if test "${SCHEMA}" != ""
+then
+    SUFFIX="-${SCHEMA}"
+else
+    SUFFIX=""
+fi
 /wait-for-it.sh -h ${CASSANDRA_NODE} -p 9042
 /go/InitKeyspace
 exec /go/bin/migrate -url cassandra://${CASSANDRA_NODE}:9042/${CASSANDRA_KEYSPACE} -path /migrations$SUFFIX $@
